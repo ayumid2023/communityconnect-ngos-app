59e7e305464d6d6a7a5ef2ee5f5dbb291d6d243b
ayumid2023 <141483735+ayumid2023@users.noreply.github.com>
2026-09-02 19:13:38 +0300

    Create jest.config.js

---
+++ server/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/scripts/**/*.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.js'],
+  testTimeout: 30000,
+};
